5a91286d335c86a7108b0c19f3c0941a07d5a5b3
matthewyu\matthew <matthewyu@welltend.com.tw>
2026-04-13 15:54:36 +0800

    update

--- blog/Program.cs
+++ blog/Program.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using blog.Entities;
+using blog.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,8 +12,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 builder.Services.AddDbContext<BlogContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<UserService>();
 
 builder.Services.AddAutoMapper(
     (IMapperConfigurationExpression cfg) => { },
