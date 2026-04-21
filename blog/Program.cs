9f29e6cfe93f302fad6c6a7db388a1f4663e5ca9
matthewyu\matthew <matthewyu@welltend.com.tw>
2026-04-21 14:06:03 +0800

    update program

--- blog/Program.cs
+++ blog/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<BlogContext>(options =>
 
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<PostService>();
+builder.Services.AddHttpClient<AiService>();
 
 builder.Services.AddAutoMapper(
     (IMapperConfigurationExpression cfg) => { },
