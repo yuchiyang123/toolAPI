a403a03300b7b847db116c3b709a0294ad2dcc22
matthewyu\matthew <matthewyu@welltend.com.tw>
2026-04-29 17:59:38 +0800

    feat. tag mapping

--- blog/Entities/Blog/PostsTag.cs
+++ blog/Entities/Blog/PostsTag.cs
@@ -1,10 +1,13 @@
-﻿namespace blog.Entities.Blog
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace blog.Entities.Blog
 {
+    [Table("PostsTag")]
     public class PostsTag
     {
         public int Id { get; set; }
-        public int FK_PostsId { get; set; }
         public required string Tag { get; set; }
-        public Posts Posts { get; set; }
+        public DateTime CreateDate { get; set; }
+        public ICollection<PostsTagMapping> PostsTagMapping { get; set; }
     }
 }
---
+++ blog/Entities/Blog/PostsTagMapping.cs
@@ -0,0 +1,11 @@
+﻿namespace blog.Entities.Blog
+{
+    public class PostsTagMapping
+    {
+        public int Id { get; set; }
+        public int FK_PostsId { get; set; }
+        public int FK_TagId { get; set; }
+        public Posts Posts { get; set; }
+        public PostsTag PostsTag { get; set; }
+    }
+}
