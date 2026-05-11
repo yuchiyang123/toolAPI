19b96fb830a106c3d232b5e967fe50b054e07c09
yuchiyang123 <torry0402@gmail.com>
2026-05-11 15:57:40 +0800

    fix. update datebase

--- blog/Migrations/20260511075622_update_filemappging.Designer.cs
+++ blog/Migrations/20260511075622_update_filemappging.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using blog.Entities;
 
@@ -11,9 +12,11 @@ using blog.Entities;
 namespace blog.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20260511075622_update_filemappging")]
+    partial class update_filemappging
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ blog/Migrations/20260511075622_update_filemappging.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blog.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_filemappging : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeDetailMapping_RecipeId",
+                table: "RecipeDetailMapping");
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecipeFileMapping",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RecipeId = table.Column<int>(type: "int", nullable: false),
+                    FileId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeFileMapping", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RecipeFileMapping_Files_FileId",
+                        column: x => x.FileId,
+                        principalTable: "Files",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RecipeFileMapping_Recipe_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipe",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeDetailMapping_RecipeId",
+                table: "RecipeDetailMapping",
+                column: "RecipeId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeFileMapping_FileId",
+                table: "RecipeFileMapping",
+                column: "FileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeFileMapping_RecipeId",
+                table: "RecipeFileMapping",
+                column: "RecipeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecipeFileMapping");
+
+            migrationBuilder.DropTable(
+                name: "Files");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeDetailMapping_RecipeId",
+                table: "RecipeDetailMapping");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeDetailMapping_RecipeId",
+                table: "RecipeDetailMapping",
+                column: "RecipeId");
+        }
+    }
+}
