dea3a9683df762f463f5f45b83847eea406476f4
matthewyu\matthew <matthewyu@welltend.com.tw>
2026-05-11 15:55:54 +0800

    fix. referce primary setting error

--- blog/Entities/BlogContext.cs
+++ blog/Entities/BlogContext.cs
@@ -200,8 +200,8 @@ namespace blog.Entities
                 entity.Property(x => x.RecipeId).IsRequired();
                 entity.Property(x => x.FileId).IsRequired();
 
-                entity.HasOne(e => e.Recipe).WithMany(e => e.RecipeFileMappings).HasForeignKey(e => e.RecipeId).HasPrincipalKey(e => e.RecipeFileMappings).OnDelete(DeleteBehavior.Cascade);
-                entity.HasOne(e => e.Files).WithMany(e => e.RecipeFileMappings).HasForeignKey(e => e.FileId).HasPrincipalKey(e => e.RecipeFileMappings).OnDelete(DeleteBehavior.Cascade);
+                entity.HasOne(e => e.Recipe).WithMany(e => e.RecipeFileMappings).HasForeignKey(e => e.RecipeId).HasPrincipalKey(e => e.Id).OnDelete(DeleteBehavior.Cascade);
+                entity.HasOne(e => e.Files).WithMany(e => e.RecipeFileMappings).HasForeignKey(e => e.FileId).HasPrincipalKey(e => e.Id).OnDelete(DeleteBehavior.Cascade);
             });
             #endregion
         }
