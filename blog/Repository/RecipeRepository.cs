9593eaa631d63e32d089f78478bca6cac0084d6f
matthewyu\matthew <matthewyu@welltend.com.tw>
2026-05-12 16:55:35 +0800

    fix. recipe navigation not include file

--- blog/Repository/RecipeRepository.cs
+++ blog/Repository/RecipeRepository.cs
@@ -19,6 +19,8 @@ namespace blog.Repository
                             .ThenInclude(x => x.RecipeIngredientsDetail)
                 .Include(x => x.RecipeStepMappings)
                     .ThenInclude(x => x.RecipeStep)
+                .Include(x => x.RecipeFileMappings)
+                    .ThenInclude(x => x.Files)
                 .AsQueryable();
         }
     }
