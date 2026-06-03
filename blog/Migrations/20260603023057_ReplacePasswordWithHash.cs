af5e323c2f37281ecd7acd470a433a398ff76540
yuchiyang123 <torry0402@gmail.com>
2026-06-03 10:33:10 +0800

    fix update migrations

---
+++ blog/Migrations/20260603023057_ReplacePasswordWithHash.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blog.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplacePasswordWithHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeFileMapping_FileId",
+                table: "RecipeFileMapping");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeFileMapping_RecipeId",
+                table: "RecipeFileMapping");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "PasswordHash");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeFileMapping_FileId",
+                table: "RecipeFileMapping",
+                column: "FileId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeFileMapping_RecipeId",
+                table: "RecipeFileMapping",
+                column: "RecipeId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeFileMapping_FileId",
+                table: "RecipeFileMapping");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeFileMapping_RecipeId",
+                table: "RecipeFileMapping");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "Password");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeFileMapping_FileId",
+                table: "RecipeFileMapping",
+                column: "FileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeFileMapping_RecipeId",
+                table: "RecipeFileMapping",
+                column: "RecipeId");
+        }
+    }
+}
