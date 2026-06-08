07ab7fd5193a4c9e3075efcd9d69665007fc145b
yuchiyang123 <torry0402@gmail.com>
2026-06-08 18:52:20 +0800

    update migrations

---
+++ blog/Migrations/20260608105141_flow_Defalt_Date.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blog.Migrations
+{
+    /// <inheritdoc />
+    public partial class flow_Defalt_Date : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FlowEdges_FlowNodes_FlowNodeId",
+                table: "FlowEdges");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FlowEdges_FlowNodeId",
+                table: "FlowEdges");
+
+            migrationBuilder.DropColumn(
+                name: "FlowNodeId",
+                table: "FlowEdges");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "FlowVersions",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "FlowVersions",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Flows",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "Flows",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "FlowNodes",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "FlowNodes",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "FlowEdges",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "FlowEdges",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "FlowVersions",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "FlowVersions",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Flows",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "Flows",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "FlowNodes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "FlowNodes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "FlowEdges",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateDate",
+                table: "FlowEdges",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FlowNodeId",
+                table: "FlowEdges",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlowEdges_FlowNodeId",
+                table: "FlowEdges",
+                column: "FlowNodeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FlowEdges_FlowNodes_FlowNodeId",
+                table: "FlowEdges",
+                column: "FlowNodeId",
+                principalTable: "FlowNodes",
+                principalColumn: "Id");
+        }
+    }
+}
