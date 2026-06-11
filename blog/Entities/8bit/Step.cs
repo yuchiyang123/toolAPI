0f26ab65c25cbdb7479d93976f32a287c6b440ba
matthewyu\matthew <matthewyu@welltend.com.tw>
2026-06-11 12:26:43 +0800

    fix(8bit). step hz not allow nullable

--- blog/Entities/8bit/Step.cs
+++ blog/Entities/8bit/Step.cs
@@ -6,7 +6,7 @@
         public int TrackId { get; set; }
         public int StepSeq { get; set; }
         public bool IsOn { get; set; }
-        public decimal Hz { get; set; }
+        public decimal? Hz { get; set; }
         public Track Track { get; set; }
     }
 }
