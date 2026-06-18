17ccf754fa8cb4fcfb8837aad6b4e55e0b839996
yuchiyang123 <torry0402@gmail.com>
2026-06-18 11:29:29 +0800

    build(migrations).

---
+++ blog/Migrations/20260618032847_addProblemTags_new.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blog.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProblemTags_new : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Difficulty",
+                table: "Problems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ProblemTags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProblemId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProblemTags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProblemTags_Problems_ProblemId",
+                        column: x => x.ProblemId,
+                        principalTable: "Problems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProblemTags_ProblemId",
+                table: "ProblemTags",
+                column: "ProblemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProblemTags");
+
+            migrationBuilder.DropColumn(
+                name: "Difficulty",
+                table: "Problems");
+        }
+    }
+}
